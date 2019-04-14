d4eb385ea413eb699e497005dd72fd6d3e0b4d41
gbandala <bandala@outlook.com>
2019-04-13 21:30:24 -0500

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,8 +2,8 @@ var Tour = require('../models/tour');
 
 //List Tours
 exports.inq = function (req, res) {
-   Tour.find( function (err, tour) {
-        if (err) return next(err);
+    Tour.find(function (err, tour) {
+        if (err) return console.log(err);
         res.send(tour);
         console.log("tours encontrados...");
     })
@@ -12,20 +12,30 @@ exports.inq = function (req, res) {
 //Get Tour by ID
 exports.inqId = (req, res) => {
     Tour.findById(req.params.id, (err, tour) => {
-         if (err) return next(err);
-         res.send(tour);
-         console.log("tour encontrado:"+req.params.id);
-     })
- };
+        if (err) return console.log(err);
+        res.send(tour);
+        console.log("tour encontrado:" + req.params.id);
+    })
+};
+
+exports.inqById = function (req, res) {
+    Tour.findById(req.params.id, function (err, tour) {
+        if (err) return console.log(err);
+        res.send(tour);
+        console.log("tour encontrado:" + req.params.id);
+    })
+};
 
 //Get Tour by name
-exports.inqName = (req, res) => {
-    Tour.findOne({ tourName: req.params.name }, (err, tour) => {
-         if (err) return next(err);
-         res.send(tour);
-         console.log("tour encontrado: "+req.params.name);
-     })
- };
+exports.inqByName = (req, res) => {
+    Tour.findOne({
+        tourName: req.params.name
+    }, (err, tour) => {
+        if (err) return console.log(err);
+        res.send(tour);
+        console.log("tour encontrado: " + req.params.name);
+    })
+};
 
 //POST a Tour
 exports.add = (req, res) => {
@@ -34,28 +44,27 @@ exports.add = (req, res) => {
         tourPackage: req.body.tourPackage,
         tourLength: req.body.tourLength,
         tourPrice: req.body.tourPrice,
-        tourDifficulty: req.body.tourDifficulty,
-        tourDescription: req.body.tourDescription
+        tourDifficulty: req.body.tourDifficulty
     })
-console.log(tour);
+    console.log(tour);
     tour.save(function (err, tour) {
         if (err) return console.error(err);
-       // console.log(tour.tourName + " insertado en la coleccion tours...");
+        // console.log(tour.tourName + " insertado en la coleccion tours...");
         res.send(tour.tourName + " insertado en la coleccion tours...");
-      });
+    });
 }
 // update a tour
-exports.mod = (req,res)=>{
-    Tour.findOneAndUpdate(req.params.id,{$set:req.body},(err,tour)=>{
+exports.mod = (req, res) => {
+    Tour.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, tour) => {
         if (err) return console.error(err);
-        res.send('tour:'+req.params.id+' actualizado..');
+        res.send('tour:' + req.params.id + ' actualizado..');
     });
 }
 
 // delete a tour
-exports.del = (req,res)=>{
-    Tour.findByIdAndDelete(req.params.id,(err,tour)=>{
+exports.del = (req, res) => {
+    Tour.findByIdAndDelete(req.params.id, (err, tour) => {
         if (err) return console.error(err);
-        res.send('tour:'+req.params.id+' borrado..');    
+        res.send('tour:' + req.params.id + ' borrado..');
     })
 }
