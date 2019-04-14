6d23fa289cf99e450cf2da2f655d520cf6716e21
gbandala <bandala@outlook.com>
2019-04-13 21:28:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ app.use(bodyParser.json());
 //3.- Definir paths disponibles
 app.get('/', (req, res, next) => { res.send('Mongo Express.... Porfavor use /api/tours');});
 app.get('/api/tours',TourController.inq);
+app.get('/api/tours/:id',TourController.inqById);
+app.get('/api/tours/names/:name',TourController.inqByName);
 app.post('/api/tours',TourController.add);
-app.get('/api/tours/:id',TourController.inqId);
 app.put('/api/tours/:id',TourController.mod);
 app.delete('/api/tours/:id',TourController.del);
-app.get('/api/tours/names/:name',TourController.inqName);
 //----------------------------------------------------------------------------------------
 //4.- Encender webserver y dbserver
 app.listen(port,()=>{
