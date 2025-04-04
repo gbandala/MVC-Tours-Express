e3a17c0223aa5a3148918344eb32d0885d44f7f1
Gabriel Bandala <bandala@outlook.com>
2025-04-04 10:02:12 -0600

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-const DbConnection='mongodb://UserLearner:UserLearner@cluster0-shard-00-01-xv2n8.mongodb.net:27017/learning_mongo?ssl=true&authSource=admin';
+const DbConnection='mongodb://xxxxx:xxxx@cluster0-shard-xxxxx.mongodb.net:27017/learning_mongo?ssl=true&authSource=admin';
 
 
 exports.conn=DbConnection;
